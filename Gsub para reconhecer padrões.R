21f57ae581d7762a20fd2e355e965f462998d0a9
Gustavo Bueno <89856818+GustavooBueno@users.noreply.github.com>
2021-10-12 00:30:34 -0300

    Add files via upload

---
+++ Gsub para reconhecer padrões.R
@@ -0,0 +1,5 @@
+
+lista <- c("Gustavo!", "!", ".")
+lista <- gsub("!", "DEU CERTO", lista)
+lista
+  
---
+++ Ler Excel.R
@@ -0,0 +1,3 @@
+library('readxl')
+setwd('C:/Users/Pichau/Desktop/linguagem r')
+df <- read_excel('DataFrame.xlsx', sheet = 1)
